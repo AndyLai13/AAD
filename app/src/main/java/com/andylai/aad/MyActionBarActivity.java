85ea92c7bd291cce09a64b1f28a8a24b487e3142
Andy Lai <andy.lai@tw.viewsonic.com>
2019-11-23 03:25:04 +0800

    add toobar - part III

--- app/src/main/java/com/andylai/aad/MyActionBarActivity.java
+++ app/src/main/java/com/andylai/aad/MyActionBarActivity.java
@@ -1,14 +1,48 @@
 package com.andylai.aad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MyActionBarActivity extends AppCompatActivity {
+
+	Toolbar mToolbar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_my_action_bar);
+		mToolbar = findViewById(R.id.my_toolbar);
+		setSupportActionBar(mToolbar);
+		if (getSupportActionBar() != null) {
+			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+			getSupportActionBar().setHomeButtonEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_toolbar_right, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.action_item1:
+			case R.id.action_item2:
+				Log.d("Andy", "action_item2");
+				break;
+			case R.id.action_notification:
+			case R.id.action_search:
+				Log.d("Andy", "action_search");
+				break;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
